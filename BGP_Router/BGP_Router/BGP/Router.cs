79e979721b5d681a093e870fd567b6d86ba03fa8
Suamine <vilsuominen@gmail.com>
2017-02-06 12:27:44 +0200

    Recent modifications
    Modified

---
+++ BGP_Router/BGP_Router/BGP/Router.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGP_Router
+{
+    class Router
+    {
+        public Socket listenerSocket;
+        public Socket speakerSocket;
+        public byte[] buffer = new byte[1024];
+
+        private static ResetEvent speakerStarted = new ResetEvent(true);
+
+
+
+    }
+}
