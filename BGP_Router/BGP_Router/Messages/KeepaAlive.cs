bd29e49c146b3935a32bba70523e54e879ee5e6a
Suamine <vilsuominen@gmail.com>
2017-02-06 13:27:37 +0200

    Modified Messages

---
+++ BGP_Router/BGP_Router/Messages/KeepaAlive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGP_Router.Messages
+{
+    class KeepaAlive : Structure
+
+    {
+        private short mType;
+        public KeepaAlive() : base ((short)(40), 19)
+        {
+            Type = 4;
+        }
+        public KeepaAlive(byte[] packet) : base(packet)
+        {
+
+        }
+        public short Type
+        {
+            get { return mType; }
+            set
+            {
+                mType = value;
+                writeType(value, 38);
+            }
+        }
+    }
+}
---
+++ BGP_Router/BGP_Router/Messages/Structure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGP_Router.Messages
+{
+    public abstract class Structure
+    {
+        private byte[] mBuffer;
+        public long marker;
+        public Structure(long marker, int length)
+        {
+            mBuffer = new byte[marker];
+            for (int i = 0; i < 16; i++)
+            {
+                writeMarker(marker, i * 2);
+            }
+            writeLength(length, 32);
+        }
+        public void writeMarker(long value, int offset)
+        {
+            byte[] tempBuffer = new byte[32];
+            
+        }
+    }
+}
