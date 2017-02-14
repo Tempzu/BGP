91f2a2a96eddeb6684e4097a4c20435ce94947b2
Tombrre <tamvali@utu.fi>
2017-02-14 14:33:27 +0200

    Fixed back

--- BGP_Router/BGP_Router/BGP/Router.cs
+++ BGP_Router/BGP_Router/BGP/Router.cs
@@ -1,21 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace BGP_Router
+namespace BGP_Router.BGP
 {
-    class Router
+    public class Router
     {
-        public Socket listenerSocket;
-        public Socket speakerSocket;
-        public byte[] buffer = new byte[1024];
+        private static AutoResetEvent speakerStarted = new AutoResetEvent(true);
+        private static AutoResetEvent listnerStarted = new AutoResetEvent(true);
 
-        private static ResetEvent speakerStarted = new ResetEvent(true);
+        public Socket mSocketListener;
+        public Socket mSocketSpeaker;
+        public byte[] mBuffer = new byte[1024];
 
+        private static AutoResetEvent SpeakerOn = new AutoResetEvent(true);
+        private static AutoResetEvent ListenerOn = new AutoResetEvent(true);
 
+        public void SocketListener()
+        {
+            // Create a TCP socket for incoming traffic
+            mSocketListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void SocketSpeaker()
+        {
+            // Create a TCP socket for outgoing traffic
+            mSocketSpeaker = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void BindSpeaker(string ipAddr, int port, int i)
+        {
+
+            // Start router speaker
+            SocketSpeaker();
+            // Bind socket to an IP end point with the port
+            mSocketSpeaker.Bind(new IPEndPoint(IPAddress.Parse(ipAddr), port));
+
+            // Don't start the speaker event yet. First print out info
+            SpeakerOn.WaitOne();
+
+            Console.WriteLine("Router Speaker: " + i + " IP-Address:" + IPAddress.Parse(((IPEndPoint)mSocketSpeaker.LocalEndPoint).Address.ToString())
+               + " Speaker started. It is in: " + GlobalVariables.speakerConnectionState + "state.");
+
+            // Start the speaker event
+            SpeakerOn.Set();
+
+        }
+        public void BindListener(string ipAddr, int port, int i)
+        {
+            // Start router listener
+            SocketListener();
+            // Bind socket to an IP end point with the port
+            mSocketListener.Bind(new IPEndPoint(IPAddress.Parse(ipAddr), port));
+
+            // Don't start the listener event yet. First print out info
+            ListenerOn.WaitOne();
+
+            Console.WriteLine("Router Listener: " + i + " IP-Address:" + IPAddress.Parse(((IPEndPoint)mSocketListener.LocalEndPoint).Address.ToString())
+                + " Listener started. It is in: " + GlobalVariables.listnerConnectionState + "state.");
+            //Start the listener event
+            ListenerOn.Set();
+
+        }
 
     }
 }
