7b6e404fea0b0d2236c75de46e2b1d7133d110d5
Tombrre <tamvali@utu.fi>
2017-05-02 15:41:59 +0300

    Update Listener.cs

--- BGP_Router/BGP_Router/BGP/Listener.cs
+++ BGP_Router/BGP_Router/BGP/Listener.cs
@@ -4,8 +4,6 @@ using System.Net.Sockets;
 using BGP_Router.Masiina;
 using BGP_Router.Messages;
 using System.Threading;
-using System.Text;
-using System.Collections.Generic;
 
 namespace BGP_Router.BGP
 {
@@ -21,59 +19,38 @@ namespace BGP_Router.BGP
         private static AutoResetEvent ReceivedMessage = new AutoResetEvent(true);
         private static AutoResetEvent SendKeepAlive = new AutoResetEvent(true);
         private static AutoResetEvent SendUpdate = new AutoResetEvent(true);
-        //private object mListenerSocket; //??
 
         public void Listen(int backlog)
         {
-            // listens for 500 tcp backup connection request
-            mSocketListener.Listen(backlog); //??
+            /*Enables the current socket to listen for incoming connection attempts; backlog is 
+            the maximum number of incoming connections that can be queued for acceptance. */
+            
+        mSocketListener.Listen(backlog);
         }
-        //it is implemented to accept to listen data
+        //Try to accept connection
         public void Accept()
         {
-
             try
             {
-
-                // Start an asynchronous socket to listen for connections.
-                //Console.WriteLine("Waiting for a connection...");
-
-                // Set the event to nonsignaled state.
-
-                // Begin to accept the client connection
-                // and it asks for two parameter with AsyncCallback and object (AcceptedCallback and null) null is set for object reference parameter
-
-                mSocketListener.BeginAccept(AcceptedCallback, mSocketListener); // ??
-                //acceptDone.Set();
-                //acceptDone.WaitOne();
-                // Wait until a connection is made before continuing.
-
-
+                mSocketListener.BeginAccept(AcceptedCallback, mSocketListener); 
             }
-
-
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
         }
-        // This Asynccallback is called when the connection is sucessful
+        //This Asynccallback is called when the connection is sucessful
         private void AcceptedCallback(IAsyncResult result)
         {
             try
             {
                 AcceptedConnection.WaitOne();
 
-                // Get the socket that handles the client request.
+                //Get the socket that handles the client request.
                 Socket ListenerSocket = result.AsyncState as Socket;
                 ListenerSocket = ListenerSocket.EndAccept(result);
 
-                //acceptDone.Set();
-                // Create the state object.
-                // StateObject state = new StateObject();
-                // state.workSocket = handler;
-                //allDone.Set();
-
+                //Check that the amount of total connections is not less than speaker connection amount
                 if (Variables.AllConnectionCount < Variables.ConnectionAndSpeaker.Count)
                 {
                     Variables.ListenerSocketDictionary.TryAdd(Variables.AllConnectionCount, ListenerSocket);
@@ -83,23 +60,14 @@ namespace BGP_Router.BGP
                 }
 
                 AcceptedConnection.Set();
-
-                // it is the place where we store data and this step is done to clear previous data from memory
+                //Clear buffer
                 mBuffer = new byte[1024];
-
-                // this is done to be ready to recive data 
-                // the four paramaters are buffer, the place in packet where 0 is the begining of packet, the amount of data where we set full capacity of buffer
-                // SocketFlag is none, when the data is received the next parameter defines where to send data, and last one is the connection which should be passed 
-                // to callback method
+                //Start to receive data with the set buffer parameters
+                //ReceivedCallback gets the ACK is executed from the receiver
                 ListenerSocket.BeginReceive(mBuffer, 0, mBuffer.Length, SocketFlags.None, ReceivedCallback, ListenerSocket);
-                //reciveDone.WaitOne();
-
-                //accept method is called to listen to more connection
+                //Listen to more connections with Accept();
                 Accept();
-                //acceptDone.WaitOne();
                 ReceivedMessage.WaitOne();
-
-
 
             }
             catch (Exception e)
@@ -107,6 +75,7 @@ namespace BGP_Router.BGP
                 Console.WriteLine(e.ToString());
             }
         }
+
         private void ReceivedCallback(IAsyncResult result)
         {
             try
@@ -114,50 +83,24 @@ namespace BGP_Router.BGP
 
                 // we catch that connection we send and since AsyncState is a object so we set it as Socket to get connection
                 Socket ListenerSocket = result.AsyncState as Socket;
-
-
-                // Retrieve the state object and the handler socket
-                // from the asynchronous state object.
-                //StateObject state = (StateObject)reasult.AsyncState;
-                //Socket handler = state.workSocket;
-
-                //EndReceive is used to count the amount of data received
+                //EndReceive tells the received data amount
                 int mBufferSize = ListenerSocket.EndReceive(result);
-                //reciveDone.Set();
-                //if (bufferSize == 58 || bufferSize == 40)
-                //{
-
-                //it is done to store to store the data in buffer to packet
+                //Set the packet size to be the same as the buffer size
                 byte[] mPacket = new byte[mBufferSize];
-                //reciveDone.Set();
-                // Console.WriteLine("*********************** Listener" + IPAddress.Parse(((IPEndPoint)ListenerSocket.LocalEndPoint).Address.ToString())
-                //   + "*********************** Speaker" + IPAddress.Parse(((IPEndPoint)ListenerSocket.RemoteEndPoint).Address.ToString()));
-
-                // it is done to create a shadow clone of buffer before anyone uses it
-                // this method stores the data in buffer to packet
+                //Copy the data to the packet from the buffer
                 Array.Copy(mBuffer, mPacket, mPacket.Length);
-
-
-
-                //Handle the packet
-                BuildPacket.Handle(mPacket, ListenerSocket);
-
+                //Call the packet handling method
+                HandlePackets.Handle(mPacket, ListenerSocket);
                 ReceivedMessage.Set();
-
+                //Set the listener to "received open message" status
                 FSM_Listener.BGPOpenMessageReceived(Variables.True);
-
-                //}
-                // else
-                //{
-
+                //clear buffer
                 mBuffer = new byte[1024];
                 ListenerSocket.BeginReceive(mBuffer, 0, mBuffer.Length, SocketFlags.None, ReceivedCallback, ListenerSocket);
-                // }
 
             }
             catch (ObjectDisposedException ex)
             {
-                // Don't care
                 Console.WriteLine("Listener socket is closed");
             }
             catch (Exception e)
@@ -168,24 +111,14 @@ namespace BGP_Router.BGP
 
         public void SendingOpenMsg_Speaker()
         {
-            //tracking the proper connection number and the speaker socket was the difficult task
-
             if (Variables.SendMsgCount < Variables.ListenerSocketDictionary.Count)
             {
-                //tempSocket[Variables.openMsgSendCount] = Variables.ListenerSocket_Dictionary[Variables.openMsgSendCount];
                 Socket tempSock = Variables.ListenerSocketDictionary[Variables.SendMsgCount];
                 Open openPacket = new Open(Variables.BGPVersion, Variables.SpeakerConnectionAndAS[(ushort)Variables.SendMsgCount], Variables.HoldTime,
                    "" + IPAddress.Parse(((IPEndPoint)tempSock.LocalEndPoint).Address.ToString()), Variables.OptimalParameterLength);
-
-
-                //Console.WriteLine("Sending open packet" + "verson : " + Variables.bgpVerson + "AS : " + Variables.speakerConAnd_AS[(ushort)Variables.openMsgSendCount] + "Hold Time : " + Variables.holdTime + "IP : "
-                // + IPAddress.Parse(((IPEndPoint)tempSocket[Variables.openMsgSendCount].RemoteEndPoint).Address.ToString()) + "param : " + Variables.optimalParLength);
                 mMessageType = "OPEN";
-                //Socket temSoc = tempSocket[Variables.openMsgSendCount];
-                Console.WriteLine("BGP Listener:" + IPAddress.Parse(((IPEndPoint)tempSock.LocalEndPoint).Address.ToString()) + " has send open Message !!");
+                Console.WriteLine("#### BGP Listener:" + IPAddress.Parse(((IPEndPoint)tempSock.LocalEndPoint).Address.ToString()) + " has sent an OPEN message ####");
                 SendSpeaker(openPacket.BGPmessage, tempSock, mMessageType);
-
-                //sendOpenDone.WaitOne();
 
                 Variables.SendMsgCount++;
             }
@@ -193,29 +126,12 @@ namespace BGP_Router.BGP
         }
         public void SendingKeepAliveMsg_Speaker()
         {
-            //tracking the proper connection number and the speaker socket was the difficult task
-
             if (Variables.KeepAliveMsgSendCount < Variables.ListenerSocketDictionary.Count)
             {
-
-
-
-                //tempSocket[Variables.keepAliveMsgSendCount] = Variables.ListenerSocket_Dictionary[Variables.keepAliveMsgSendCount];
                 Socket tempSock = Variables.ListenerSocketDictionary[Variables.KeepAliveMsgSendCount];
                 KeepAlive keepAlivePacket = new KeepAlive();
-                mMessageType = "KeepAlive";
-
-                //sendKeepAliveMessage.WaitOne();
-
-                //Socket temSoc = tempSocket[Variables.keepAliveMsgSendCount];
-                Console.WriteLine("BGP Listener:" + IPAddress.Parse(((IPEndPoint)tempSock.LocalEndPoint).Address.ToString()) + " has send keepAlive Message !!");
-
-
-
+                Console.WriteLine("#### BGP Listener:" + IPAddress.Parse(((IPEndPoint)tempSock.LocalEndPoint).Address.ToString()) + " has sent a KEEPALIVE message ####");
                 SendSpeaker(keepAlivePacket.BGPmessage, tempSock, mMessageType);
-                //sendKeepAliveMessage.Set();
-
-                //sendKeepAliveDone.WaitOne();
 
                 Variables.KeepAliveMsgSendCount++;
 
@@ -229,19 +145,12 @@ namespace BGP_Router.BGP
             {
                 if (Variables.ListenerSocketDictionary.ContainsKey(Variables.KeepAliveExpiredCount))
                 {
-
-
-
                     Socket tempSock = Variables.ListenerSocketDictionary[Variables.KeepAliveExpiredCount];
                     KeepAlive keepAlivePacket = new KeepAlive();
                     mMessageType = "KeepAlive";
-
                     SendKeepAlive.WaitOne();
-
-                    Console.WriteLine("BGP Listener:" + IPAddress.Parse(((IPEndPoint)tempSock.LocalEndPoint).Address.ToString()) + " has send keepAlive Message !!");
-
+                    Console.WriteLine("#### BGP Listener:" + IPAddress.Parse(((IPEndPoint)tempSock.LocalEndPoint).Address.ToString()) + " has sent a KEEPALIVE expired message ####");
                     SendKeepAlive.Set();
-
                     SendSpeaker(keepAlivePacket.BGPmessage, tempSock, mMessageType);
                     Variables.KeepAliveExpiredCount++;
                 }
@@ -261,26 +170,16 @@ namespace BGP_Router.BGP
         {
             try
             {
-
                 Socket ListenerSocket = sendSock;
-
                 if (msg == "OPEN")
                 {
-
                     mMessageType = msg;
-                    //FSM_Listener.BGPKeepAliveMsgSend(Variables.True);
                     ListenerSocket.BeginSend(data, 0, data.Length, 0, SendCallback, sendSock);
-                    //sendOpenDone.Set();
-
                 }
                 else if (msg == "KeepAlive")
                 {
-
                     mMessageType = msg;
                     ListenerSocket.BeginSend(data, 0, data.Length, 0, SendCallback, sendSock);
-                    //sendKeepAliveDone.Set();
-                    //FSM_Listener.BGPKeepAliveMsgSend(Variables.True);
-
                 }
                 else if (msg == "Update")
                 {
@@ -288,13 +187,11 @@ namespace BGP_Router.BGP
                     ListenerSocket.BeginSend(data, 0, data.Length, 0, SendCallback, sendSock);
 
                     SendUpdate.WaitOne();
-                    //Console.WriteLine("Listener Send update message to speeker");
                 }
                 else if (msg == "Notify")
                 {
                     mMessageType = msg;
                     ListenerSocket.BeginSend(data, 0, data.Length, 0, SendCallback, sendSock);
-                    //Console.WriteLine("Listener Send update message to speeker");
                 }
 
 
@@ -313,20 +210,9 @@ namespace BGP_Router.BGP
 
                 Socket ListenerSocket = result.AsyncState as Socket;
 
-                // Complete sending the data to the remote device.
+                //Finish the data transmission
                 int bytesSent = ListenerSocket.EndSend(result);
                 SendUpdate.Set();
-
-
-
-
-                //sendOpenDone.Set();
-                //sendKeepAliveDone.Set();
-
-
-                //sendDone.Set();
-                //FSM_Listener.BGPOpenMsgSent(Variables.True);
-
             }
             catch (Exception e)
             {
