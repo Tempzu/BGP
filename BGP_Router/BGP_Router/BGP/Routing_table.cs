24c4d420244b98b0355beb0386843d2c2c6111a9
Suamine <vilsuominen@gmail.com>
2017-05-02 16:18:31 +0300

    Commenting routing table

--- BGP_Router/BGP_Router/BGP/Routing_table.cs
+++ BGP_Router/BGP_Router/BGP/Routing_table.cs
@@ -13,15 +13,15 @@ namespace BGP_Router.BGP
         public static DataTable GetTable()
         {
             Variables.ConnectionSpeakerAs_ListenerAs.Clear();
-            // Here we create a DataTable with four columns.
-            DataTable Route_table = new DataTable();
-            Route_table.Columns.Add("Connection", typeof(int));
-            Route_table.Columns.Add("Network", typeof(string));
-            Route_table.Columns.Add("AS_Number", typeof(int));
-            Route_table.Columns.Add("NextHop", typeof(string));
-            Route_table.Columns.Add("AS_NextHop", typeof(int));
-            Route_table.Columns.Add("IGP/EGP", typeof(int));
-
+            // Here we create a DataTable with five columns.
+            DataTable Routing_Table = new DataTable();
+            Routing_Table.Columns.Add("Connection", typeof(int));
+            Routing_Table.Columns.Add("Network", typeof(string));
+            Routing_Table.Columns.Add("AS_Number", typeof(int));
+            Routing_Table.Columns.Add("NextHop", typeof(string));
+            Routing_Table.Columns.Add("AS_NextHop", typeof(int));
+            
+            // Method for finding pairs to each peer in the system. Creates the routing table with the pre-set entities.
             foreach (KeyValuePair<int, string> pair in Variables.ConnectionAndListener)
             {
                 try
@@ -29,22 +29,19 @@ namespace BGP_Router.BGP
                     if (Variables.ListenerAS[pair.Value] == Variables.SpeakerAS[Variables.ConnectionAndSpeaker[pair.Key]])
                     {
 
-                        //Console.WriteLine("{0},{1}", pair.Key, pair.Value);
-                        Route_table.Rows.Add(pair.Key, Variables.ConnectionAndSpeaker[pair.Key], Variables.SpeakerAS[Variables.ConnectionAndSpeaker[pair.Key]],
+                        Routing_Table.Rows.Add(pair.Key, Variables.ConnectionAndSpeaker[pair.Key], Variables.SpeakerAS[Variables.ConnectionAndSpeaker[pair.Key]],
                           pair.Value, Variables.ListenerAS[pair.Value], 0);
-                        //Storing connection speaker (ip, AS) and listner (ip, As)
                         Tuple<string, ushort, string, ushort> conSpeakerAs_ListnerAs = new Tuple<string, ushort, string, ushort>(Variables.ConnectionAndSpeaker[pair.Key],
                            Variables.SpeakerAS[Variables.ConnectionAndSpeaker[pair.Key]], pair.Value, Variables.ListenerAS[pair.Value]);
                         Variables.ConnectionSpeakerAs_ListenerAs.Add(pair.Key, conSpeakerAs_ListnerAs);
                     }
                     else
                     {
-                        Route_table.Rows.Add(pair.Key, Variables.ConnectionAndSpeaker[pair.Key], Variables.SpeakerAS[Variables.ConnectionAndSpeaker[pair.Key]],
+                        Routing_Table.Rows.Add(pair.Key, Variables.ConnectionAndSpeaker[pair.Key], Variables.SpeakerAS[Variables.ConnectionAndSpeaker[pair.Key]],
                             pair.Value, Variables.ListenerAS[pair.Value], 1);
-                        //Storing connection speaker (ip, AS) and listner (ip, As)
-                        Tuple<string, ushort, string, ushort> conSpeakerAs_ListnerAs = new Tuple<string, ushort, string, ushort>(Variables.ConnectionAndSpeaker[pair.Key],
+                        Tuple<string, ushort, string, ushort> connectionSpeakerAs_ListenerAs = new Tuple<string, ushort, string, ushort>(Variables.ConnectionAndSpeaker[pair.Key],
                            Variables.SpeakerAS[Variables.ConnectionAndSpeaker[pair.Key]], pair.Value, Variables.ListenerAS[pair.Value]);
-                        Variables.ConnectionSpeakerAs_ListenerAs.Add(pair.Key, conSpeakerAs_ListnerAs);
+                        Variables.ConnectionSpeakerAs_ListenerAs.Add(pair.Key, connectionSpeakerAs_ListenerAs);
                     }
                 }
                 catch (Exception e)
@@ -54,23 +51,20 @@ namespace BGP_Router.BGP
 
             }
 
-            return Route_table;
+            return Routing_Table;
         }
 
+        // Simple methods for printing out the corresponding routing tables to each AS.
         public void DisplayDataAS1()
         {
-            // This uses the GetTable method (please paste it in).
-            //GlobalVariables.data = GetTable();
-
-            Console.WriteLine("BGP ROUTING TABLE OF AS 1");
-            Console.WriteLine("Connection" + "   Network   " + " AS_Number " + "   NextHop  " + " AS_NextHop ");
-            // ... Loop over all rows.
+        
+            Console.WriteLine("BGP routing table for AS1");
+            Console.WriteLine("Connection" + "   Network   " + " AS Number " + "   NextHop  " + " AS NextHop ");
+            // Loop for filling out each rows with corresponding information.
             foreach (DataRow row in Variables.Data.Rows)
             {
                 if (row.Field<int>(2) == 1 || row.Field<int>(4) == 1)
                 {
-                    // ... Write value of first field as integer.
-
                     Console.WriteLine("     " + row.Field<int>(0) + "       " + row.Field<string>(1) + "  " + row.Field<int>(2) + "     "
                         + row.Field<string>(3) + "    " + row.Field<int>(4) + "       " + row.Field<int>(5));
                 }
@@ -79,17 +73,14 @@ namespace BGP_Router.BGP
         }
         public void DisplayDataAS2()
         {
-            // This uses the GetTable method (please paste it in).
-
-            Console.WriteLine("BGP ROUTING TABLE OF AS2");
-            Console.WriteLine("Connection" + "   Network   " + " AS_Number " + "   NextHop  " + " AS_NextHop ");
-            // ... Loop over all rows.
+            Console.WriteLine("BGP routing table for AS2");
+            Console.WriteLine("Connection" + "   Network   " + " A Number " + "   NextHop  " + " AS NextHop ");
+            // Loop for filling out each rows with corresponding information.
             foreach (DataRow row in Variables.Data.Rows)
             {
                 if (row.Field<int>(2) == 2 || row.Field<int>(4) == 2)
                 {
-                    // ... Write value of first field as integer.
-
+                   
                     Console.WriteLine("     " + row.Field<int>(0) + "       " + row.Field<string>(1) + "  " + row.Field<int>(2) + "     "
                         + row.Field<string>(3) + "    " + row.Field<int>(4) + "       " + row.Field<int>(5));
                 }
@@ -97,17 +88,14 @@ namespace BGP_Router.BGP
         }
         public void DisplayDataAS3()
         {
-            // This uses the GetTable method (please paste it in).
-
-            Console.WriteLine("BGP ROUTING TABLE OF AS3");
-            Console.WriteLine("Connection" + "   Network   " + " AS_Number " + "   NextHop  " + " AS_NextHop ");
-            // ... Loop over all rows.
+            Console.WriteLine("BGP routing table for AS3");
+            Console.WriteLine("Connection" + "   Network   " + " AS Number " + "   NextHop  " + " AS NextHop ");
+            // Loop for filling out each rows with corresponding information.
             foreach (DataRow row in Variables.Data.Rows)
             {
                 if (row.Field<int>(2) == 3 || row.Field<int>(4) == 3)
                 {
-                    // ... Write value of first field as integer.
-
+                   
                     Console.WriteLine("     " + row.Field<int>(0) + "       " + row.Field<string>(1) + "  " + row.Field<int>(2) + "     "
                         + row.Field<string>(3) + "    " + row.Field<int>(4) + "       " + row.Field<int>(5));
                 }
