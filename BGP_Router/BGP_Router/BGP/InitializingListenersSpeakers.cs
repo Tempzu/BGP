7fbb79c9b4078484e2a0c8139b2a3be59a2ceec5
Tempzu <ttlaakso@gmail.com>
2017-05-05 01:29:08 +0300

    Apparently I had forgotten the change before. Well here it is now.

--- BGP_Router/BGP_Router/BGP/InitializingListenersSpeakers.cs
+++ BGP_Router/BGP_Router/BGP/InitializingListenersSpeakers.cs
@@ -27,29 +27,29 @@ namespace BGP_Router.BGP
             temp = Console.ReadLine();
             try
             {
-                as1Last = int.Parse(temp) - 1; //-1 since routers start from 0 
-                if (as1Last < 0)
+                Variables.as1Last = int.Parse(temp) - 1; //-1 since routers start from 0 
+                if (Variables.as1Last < 0)
                 {
                     Console.WriteLine("The character you typed wasn't in the given bounds, so we set 1 router in AS1.");
-                    as1Last = 0;
+                    Variables.as1Last = 0;
                 }
-                else if (as1Last > 7)
+                else if (Variables.as1Last > 7)
                 {
                     Console.WriteLine("The character you typed wasn't in the given bounds, so we set 8 routers in AS1.");
-                    as1Last = 7;
+                    Variables.as1Last = 7;
                 }
             }
             catch
             {
                 Console.WriteLine("The character you typed wasn't a number, so we set 3 routers to AS1.");
-                as1Last = 2;
+                Variables.as1Last = 2;
             }
-            if (as1Last < 7)
+            if (Variables.as1Last < 7)
             {
-                as2First = as1Last + 1;
-                if (as1Last > 0)
+                Variables.as2First = Variables.as1Last + 1;
+                if (Variables.as1Last > 0)
                 {
-                    Console.WriteLine("How many routers do you want in AS2? (1-" + (8 - as1Last) + "). Rest will be placed in AS3.");
+                    Console.WriteLine("How many routers do you want in AS2? (1-" + (8 - Variables.as1Last) + "). Rest will be placed in AS3.");
                 }
                 else
                 {
@@ -58,16 +58,16 @@ namespace BGP_Router.BGP
                 temp = Console.ReadLine();
                 try
                 {
-                    as2Last = as1Last + int.Parse(temp);
-                    if (as2Last <= as1Last)
+                    Variables.as2Last = Variables.as1Last + int.Parse(temp);
+                    if (Variables.as2Last <= Variables.as1Last)
                     {
                         Console.WriteLine("The character you typed wasn't in the given bounds, so we set 1 router in AS2.");
-                        as2Last = as1Last + 1;
+                        Variables.as2Last = Variables.as1Last + 1;
                     }
-                    else if (as2Last > 8)
+                    else if (Variables.as2Last > 8)
                     {
                         Console.WriteLine("The character you typed wasn't in the given bounds, so we set 1 router in the AS3 and rest in AS2.");
-                        as2Last = 8;
+                        Variables.as2Last = 8;
                     }
                 }
                 catch
@@ -75,14 +75,14 @@ namespace BGP_Router.BGP
                     Console.WriteLine("The character you typed wasn't a number, so we set " + (8 - Variables.as1Last) + " routers to AS2.");
                     Variables.as2Last = Variables.as1Last + (8 - Variables.as1Last);
                 }
-                as3First = as2Last + 1;
+                Variables.as3First = Variables.as2Last + 1;
             }
             else
             {
                 Console.WriteLine("One router will be now placed in both remaining AS.");
-                as2First = 8;
-                as2Last = as2First;
-                as3First = 9;
+                Variables.as2First = 8;
+                Variables.as2Last = Variables.as2First;
+                Variables.as3First = 9;
             }
             Console.WriteLine("\nConnections will be now created. Below you can see available commands.\n");
             Console.WriteLine("Type 'update' to enforce local policy. This must be done in order to update routing tables.");
@@ -97,7 +97,7 @@ namespace BGP_Router.BGP
             {
 
                 BGPListener[i] = new Listener();
-                if (i < as2First)
+                if (i < Variables.as2First)
                 {
                     AS = Variables.AS1;
                     Variables.ListenerConnectionAndAS.TryAdd((ushort)i, AS);
@@ -106,7 +106,7 @@ namespace BGP_Router.BGP
 
 
                 }
-                else if (i > as1Last && i < as3First)
+                else if (i > Variables.as1Last && i < Variables.as3First)
                 {
                     AS = Variables.AS2;
                     Variables.ListenerConnectionAndAS.TryAdd((ushort)i, AS);
@@ -114,7 +114,7 @@ namespace BGP_Router.BGP
                     Variables.ListenerAS.TryAdd(Variables.As2_IP_Prefix + i, AS);
 
                 }
-                else if (i > as2First && i < 10)
+                else if (i > Variables.as2First && i < 10)
                 {
                     AS = Variables.AS3;
                     Variables.ListenerConnectionAndAS.TryAdd((ushort)i, AS);
@@ -150,10 +150,10 @@ namespace BGP_Router.BGP
 
                 BGPSpeaker[m] = new Speaker();
 
-                if (k < as2First)
+                if (k < Variables.as2First)
                 {
                     AS = Variables.AS1;
-                    if (k == as1Last)
+                    if (k == Variables.as1Last)
                     {
 
                         BGPSpeaker[m].BindSpeaker(Variables.As1_IP_Prefix + k, Variables.SpeakerPortNumber, m);
@@ -164,7 +164,7 @@ namespace BGP_Router.BGP
                         BGPSpeaker[m].BindSpeaker(Variables.As1_IP_Prefix + k, Variables.SpeakerPortNumber + 1, m);
 
                     }
-                    else if (k < as1Last)
+                    else if (k < Variables.as1Last)
                     {
 
                         BGPSpeaker[m].BindSpeaker(Variables.As1_IP_Prefix + k, Variables.SpeakerPortNumber, m);
@@ -173,10 +173,10 @@ namespace BGP_Router.BGP
                     }
 
                 }
-                else if (k > as1Last && k < as3First)
+                else if (k > Variables.as1Last && k < Variables.as3First)
                 {
                     AS = Variables.AS2;
-                    if (k == as2First)
+                    if (k == Variables.as2First)
                     {
                         BGPSpeaker[m].BindSpeaker(Variables.As2_IP_Prefix + k, Variables.SpeakerPortNumber, m);
                         Variables.SpeakerAS.TryAdd(Variables.As2_IP_Prefix + k, AS);
@@ -184,7 +184,7 @@ namespace BGP_Router.BGP
                         BGPSpeaker[m] = new Speaker();
                         BGPSpeaker[m].BindSpeaker(Variables.As2_IP_Prefix + k, Variables.SpeakerPortNumber + 1, m);
                     }
-                    else if (k == as2Last)
+                    else if (k == Variables.as2Last)
                     {
                         BGPSpeaker[m].BindSpeaker(Variables.As2_IP_Prefix + k, Variables.SpeakerPortNumber, m);
                         Variables.SpeakerAS.TryAdd(Variables.As2_IP_Prefix + k, AS);
@@ -195,7 +195,7 @@ namespace BGP_Router.BGP
                         BGPSpeaker[m] = new Speaker();
                         BGPSpeaker[m].BindSpeaker(Variables.As2_IP_Prefix + k, Variables.SpeakerPortNumber + 2, m);
                     }
-                    else if (as2First < k && k < as2Last)
+                    else if (Variables.as2First < k && k < Variables.as2Last)
                     {
                         BGPSpeaker[m].BindSpeaker(Variables.As2_IP_Prefix + k, Variables.SpeakerPortNumber, m);
                         Variables.SpeakerAS.TryAdd(Variables.As2_IP_Prefix + k, AS);
@@ -203,7 +203,7 @@ namespace BGP_Router.BGP
 
 
                 }
-                else if (k > as2Last && k < 9)
+                else if (k > Variables.as2Last && k < 9)
                 {
                     AS = Variables.AS3;
 
@@ -236,12 +236,12 @@ namespace BGP_Router.BGP
             {
 
                 //creating speakers to AS1
-                if (k < as2First)
+                if (k < Variables.as2First)
                 {
 
                     Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS1);
                     Variables.ListenerConnectionCount = n;
-                    if (k == as1Last) //creating connection which connects to the first router of AS2
+                    if (k == Variables.as1Last) //creating connection which connects to the first router of AS2
                     {
                         BGPSpeaker[n].Connect(Variables.As2_IP_Prefix + (k + 1), Variables.ListenerPortNumber, k, k + 1);
 
@@ -249,7 +249,7 @@ namespace BGP_Router.BGP
                         Variables.ConnectionAndSpeaker.TryAdd(n, Variables.As1_IP_Prefix + k);
                         SendOpenMessageToListener(n);
 
-                        if (1 < as1Last) //more connections are created to AS1 depending on the size of AS1
+                        if (1 < Variables.as1Last) //more connections are created to AS1 depending on the size of AS1
                         {
                             n++;
                             Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS1);
@@ -260,7 +260,7 @@ namespace BGP_Router.BGP
                         }
 
                     }
-                    else if (0 < as1Last) //creating normal connections to AS1 (n -> n+1 )
+                    else if (0 < Variables.as1Last) //creating normal connections to AS1 (n -> n+1 )
                     {
                         BGPSpeaker[n].Connect(Variables.As1_IP_Prefix + (k + 1), Variables.ListenerPortNumber, k, k + 1);
                         Variables.ConnectionAndListener.TryAdd(n, Variables.As1_IP_Prefix + (k + 1));
@@ -272,18 +272,18 @@ namespace BGP_Router.BGP
 
 
                 }
-                else if (k > as1Last && k < as3First) //creating connections to AS2
+                else if (k > Variables.as1Last && k < Variables.as3First) //creating connections to AS2
                 {
 
                     Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS2);
                     Variables.ListenerConnectionCount = n;
-                    if (k == as2First && (as2Last - as2First) > 0) //more connections are created to AS2 depending on the size of AS2
+                    if (k == Variables.as2First && (Variables.as2Last - Variables.as2First) > 0) //more connections are created to AS2 depending on the size of AS2
                     {
                         BGPSpeaker[n].Connect(Variables.As2_IP_Prefix + (k + 1), Variables.ListenerPortNumber, k, k + 1);
                         Variables.ConnectionAndListener.TryAdd(n, Variables.As2_IP_Prefix + (k + 1));
                         Variables.ConnectionAndSpeaker.TryAdd(n, Variables.As2_IP_Prefix + k);
                         SendOpenMessageToListener(n);
-                        if (k == as2First && (as2Last - as2First) > 1)
+                        if (k == Variables.as2First && (Variables.as2Last - Variables.as2First) > 1)
                         {
                             n++;
                             Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS2);
@@ -294,7 +294,7 @@ namespace BGP_Router.BGP
                         }
                     }
 
-                    else if (k == as2Last) //creating connection which connects to the first router of AS3
+                    else if (k == Variables.as2Last) //creating connection which connects to the first router of AS3
                     {
                         BGPSpeaker[n].Connect(Variables.As3_IP_Prefix + (k + 1), Variables.ListenerPortNumber, k, k + 1);
                         Variables.ConnectionAndListener.TryAdd(n, Variables.As3_IP_Prefix + (k + 1));
@@ -302,7 +302,7 @@ namespace BGP_Router.BGP
                         SendOpenMessageToListener(n);
 
 
-                        if ((as2Last - as2First) > 1) //more connections are created to AS2 depending on the size of AS2
+                        if ((Variables.as2Last - Variables.as2First) > 1) //more connections are created to AS2 depending on the size of AS2
                         {
                             n++;
                             Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS2);
@@ -310,7 +310,7 @@ namespace BGP_Router.BGP
                             Variables.ConnectionAndListener.TryAdd(n, Variables.As2_IP_Prefix + (k - 2));
                             Variables.ConnectionAndSpeaker.TryAdd(n, Variables.As2_IP_Prefix + k);
                             SendOpenMessageToListener(n);
-                            if ((as2Last - as2First) > 2)
+                            if ((Variables.as2Last - Variables.as2First) > 2)
                             {
                                 n++;
                                 Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS2);
@@ -322,7 +322,7 @@ namespace BGP_Router.BGP
                         }
 
                     }
-                    else if (as2First < k && k < as2Last) //creating normal connections to AS2 (n -> n+1 )
+                    else if (Variables.as2First < k && k < Variables.as2Last) //creating normal connections to AS2 (n -> n+1 )
                     {
                         BGPSpeaker[n].Connect(Variables.As2_IP_Prefix + (k + 1), Variables.ListenerPortNumber, k, k + 1);
 
@@ -336,7 +336,7 @@ namespace BGP_Router.BGP
 
                 }
 
-                else if (k > as2Last && k < 9)
+                else if (k > Variables.as2Last && k < 9)
                 {
 
                     Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS3);
@@ -355,7 +355,7 @@ namespace BGP_Router.BGP
                     Variables.ConnectionAndListener.TryAdd(n, Variables.As1_IP_Prefix + (k - 9));
                     Variables.ConnectionAndSpeaker.TryAdd(n, Variables.As3_IP_Prefix + k);
                     SendOpenMessageToListener(n);
-                    if ((k - (as2Last + 1)) > 1)//more connections are created to AS3 depending on the size of AS3
+                    if ((k - (Variables.as2Last + 1)) > 1)//more connections are created to AS3 depending on the size of AS3
                     {
                         n++;
                         Variables.SpeakerConnectionAndAS.TryAdd((ushort)n, Variables.AS3);
